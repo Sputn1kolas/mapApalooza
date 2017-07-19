35e033c209994fddd8240ac189426b2d66d92e81
Sputn1kolas <nikolas.clark@mail.mcgill.ca>
2017-07-19 22:08:59 +0000

    added sequelize etc

--- server/express_server.js
+++ server/express_server.js
@@ -13,8 +13,8 @@ const app = express()
 
 
 app.set("view engine", "ejs")
-app.use(bodyParser.urlencoded({extended: true}))
-app.use(cookieParser())
+// app.use(bodyParser.urlencoded({extended: true}))
+// app.use(cookieParser())
 app.use(cookieSession({ secret: 'Banannnas!', cookie: { maxAge: 60 * 60 * 1000 }}))
 app.use(express.static("public")) // this is where files that html references will din .
 
