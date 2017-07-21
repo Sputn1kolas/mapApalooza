05fa1a1eb1e2f717f8cedb37bbd4a85913089812
Sidney Cardozo <scooby123sid@gmail.com>
2017-07-21 21:03:26 +0000

    added the hash method to the seed file so that the passwords entered are seeded as well

--- db/seeds/seed_name.js
+++ db/seeds/seed_name.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs');
+const salt = bcrypt.genSaltSync();
+const hashedPassword = bcrypt.hashSync('password', salt);
+
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
@@ -5,9 +9,9 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, username: 'NIK!',     password: 'password',  email:'nikolas.clark@gmail.com'},
-        {id: 2, username: 'SIDNEY!',  password: 'password',  email:'Sidney.Coroso@gmail.com'},
-        {id: 3, username: 'UCHEN!',   password: 'password',  email:'UCHEN.lastname@gmail.com'}
+        {id: 1, username: 'NIK!',     password: hashedPassword,  email:'nikolas.clark@gmail.com'},
+        {id: 2, username: 'SIDNEY!',  password: hashedPassword,  email:'Sidney.Coroso@gmail.com'},
+        {id: 3, username: 'YUCHEN!',   password: hashedPassword,  email:'UCHEN.lastname@gmail.com'}
       ]);
     });
 };
