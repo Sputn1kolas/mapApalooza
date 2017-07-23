25f8bff460e9d45345942cb825b8f95f3f253c71
YuChenHeMTL <yuchen7568@gmail.com>
2017-07-23 21:21:43 +0000

    added register

--- public/scripts/login_form.js
+++ public/scripts/login_form.js
@@ -1,5 +1,5 @@
 $(document).ready(function (){
-  $(".login_form_button").not("#login_form").on("click",handler1)
+  $(".login_form_button > :not(#login_form)").on("click",handler1)
 
   function handler1() {
     $(".login_form_button").css("background", "#b000b0")
@@ -12,4 +12,18 @@ $(document).ready(function (){
     $("#login_form").slideUp(300)
       $(this).one("click", handler1);
   }
+  $(".register > :not(#register_form)").on("click",handler3)
+
+  function handler3() {
+    $(".register").css("background", "#b000b0")
+    $("#register_form").slideDown(300)
+    $(this).one("click", handler4);
+  }
+
+  function handler4() {
+    $(".register").css("background", "#a000a0")
+    $("#register_form").slideUp(300)
+      $(this).one("click", handler3);
+  }
+
 })
