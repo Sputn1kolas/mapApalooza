737aa24345e33fa1400633a9b6fe45284f92d582
YuChenHeMTL <yuchen7568@gmail.com>
2017-07-23 23:25:12 +0000

    added search results

--- public/scripts/login_form.js
+++ public/scripts/login_form.js
@@ -2,26 +2,22 @@ $(document).ready(function (){
   $(".login_form_button > :not(#login_form)").on("click",handler1)
 
   function handler1() {
-    $(".login_form_button").css("background", "#b000b0")
     $("#login_form").slideDown(300)
     $(this).one("click", handler2);
   }
 
   function handler2() {
-    $(".login_form_button").css("background", "#a000a0")
     $("#login_form").slideUp(300)
       $(this).one("click", handler1);
   }
   $(".register > :not(#register_form)").on("click",handler3)
 
   function handler3() {
-    $(".register").css("background", "#b000b0")
     $("#register_form").slideDown(300)
     $(this).one("click", handler4);
   }
 
   function handler4() {
-    $(".register").css("background", "#a000a0")
     $("#register_form").slideUp(300)
       $(this).one("click", handler3);
   }
