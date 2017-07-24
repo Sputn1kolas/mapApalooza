21c3e6b1e1be005c74fd90e4cedf590aa9c4e45b
Sidney Cardozo <scooby123sid@gmail.com>
2017-07-24 00:31:05 +0000

    added the ajax call for login

--- public/scripts/settingCookies.js
+++ public/scripts/settingCookies.js
@@ -5,18 +5,37 @@ $(document).ready(function (){
     let username = $('#register .username').val()
     let password = $('#register .pass').val()
     let email = $('#register .email').val()
+    $.ajax({
+      url:'/register',
+      type:'POST',
+      data: {
+        username: username,
+        password: password,
+        email: email
+      },
+      success: function(res) {
+        console.log("new entry added");
+      }
+    });
+  })
+
+
+  $("#login").on('click', 'button', function(event) {
+    event.preventDefault();
+    let username = $('#register .username').val()
+    let password = $('#register .pass').val()
     console.log("Client side", username, password)
     $.ajax({
-    url:'/register',
-    type:'POST',
-    data: {
-      username: username,
-      password: password,
-      email: email
-    },
-    success: function(res) {
-      console.log(res)
-    }
-  });
+      url:'/login',
+      type:'POST',
+      data: {
+        username: username,
+        password: password
+      },
+      success: function(res) {
+        console.log(res)
+      }
+    });
   })
+
 })
